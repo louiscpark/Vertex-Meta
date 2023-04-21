35a893deb0379b6e18988e2c072b7584e67a3648
louiscpark <125378607+louiscpark@users.noreply.github.com>
2023-04-22 00:18:47 +0900

    Add files via upload

---
+++ public/text/brand.js
@@ -0,0 +1,12 @@
+module.exports = {
+  agency: {
+    name: 'Agency',
+    desc: 'Oiron Agency | React Landing Page Template',
+    prefix: 'oiron',
+    footerText: 'Oiron Theme 2022',
+    logoText: 'Oiron Theme',
+    projectName: 'Agency',
+    url: 'awrora1.indisains.com/agency',
+    img: '/static/images/logo-oiron.png'
+  }
+};
